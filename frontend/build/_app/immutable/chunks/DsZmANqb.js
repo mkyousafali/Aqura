f6905541dc349a14ce2bce142545b7626108d139
mkyousafali <mk.yousafali@gmail.com>
2025-10-03 21:11:53 +0300

    Align SvelteKit and Vercel output directory configuration - Fixed SvelteKit adapter output from '../build' to 'build' - Updated Vercel outputDirectory to 'frontend/build' - Resolved build output location mismatch for deployment

---
+++ frontend/build/_app/immutable/chunks/DsZmANqb.js
@@ -0,0 +1,2 @@
+import{e}from"./CE8Bg3hM.js";e();
+//# sourceMappingURL=DsZmANqb.js.map
---
+++ frontend/build/_app/immutable/entry/start.BNYiYXbk.js
@@ -0,0 +1,2 @@
+import{l as o,a as r}from"../chunks/gpHOA_XT.js";export{o as load_css,r as start};
+//# sourceMappingURL=start.BNYiYXbk.js.map
