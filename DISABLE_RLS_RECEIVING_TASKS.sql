7a2a18abca56d6dc913729dc5f89f01beba187b3
mkyousafali <mkyousafali@example.com>
2025-12-07 02:07:48 +0300

    Cleanup: Remove all test files and temporary documentation

--- DISABLE_RLS_RECEIVING_TASKS.sql
+++
@@ -1,8 +0,0 @@
--- Disable RLS on receiving_tasks table
--- This table is used to create tasks after receiving records are created
-
-ALTER TABLE receiving_tasks DISABLE ROW LEVEL SECURITY;
-
--- Verify
-SELECT tablename, rowsecurity FROM pg_tables 
-WHERE tablename = 'receiving_tasks';
