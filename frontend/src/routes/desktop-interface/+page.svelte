d9ef3442286fcb95caaad0d42a1dbb365402535b
mkyousafali <mk.yousafali@gmail.com>
2026-02-23 06:40:16 +0300

    reset: Phase One Complete - reset all versions to AQ1.1.1.1

--- frontend/src/routes/desktop-interface/+page.svelte
+++ frontend/src/routes/desktop-interface/+page.svelte
@@ -553,7 +553,7 @@
 								✅ {$currentLocale === 'ar' ? 'محدّث' : 'Up to Date'}
 							</span>
 						{/if}
-						<button class="version-badge" on:click={showVersionInfo} title="Version Changelog">AQ2.2.2.2</button>
+						<button class="version-badge" on:click={showVersionInfo} title="Version Changelog">AQ1.1.1.1</button>
 
 						<div class="logo" on:click={handleLogoClick} role="button" tabindex="0" on:keydown={(e) => e.key === 'Enter' && handleLogoClick()}>
 							<img src="/icons/Aqura logo.png" alt="Aqura Logo" class="logo-image" />
